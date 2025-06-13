a3faecd313840324bb91553f533c7da6089d5d95
harurun866 <harurunkinako0121@gmail.com>
2025-06-13 05:39:45 +0000

    Delete a  Blog Post

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -13,6 +13,14 @@
     <a href='/posts/create'>create</a>
     <div class='posts'>
         @foreach ($posts as $post)
+        <p class='body'>{{ $post->body }}</p>
+        <!-- 以下を追記 -->
+        <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post">
+            @csrf
+            @method('DELETE')
+            <button type="button" onclick="deletePost({{ $post->id }})">delete</button>
+        </form>
+
         <div class='post'>
             <h2 class='title'>
                 <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
@@ -24,6 +32,17 @@
     <div class='paginate'>
         {{ $posts->links() }}
     </div>
+    `form_${id}`
+    <script>
+        function deletePost(id) {
+            'use strict'
+
+            if (confirm('削除すると復元できません。\n本当に削除しますか？')) {
+                document.getElementById(`form_${id}`).submit();
+            }
+        }
+    </script>
+
 </body>
 
 </html>
